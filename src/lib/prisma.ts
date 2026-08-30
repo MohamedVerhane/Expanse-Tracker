093c9a07a80a1d9d1b4eb6b4a8a7358e5f4c3ddc
Mohamed Verhane <mohamedmv.1996@gmail.com>
2026-08-30 13:58:24 +0100

    Edit package.json and prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,12 +3,13 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const prismaClientSingleton = () => {
-  const connectionUrl =
-    process.env.DATABASE_URL ??
-    `mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
-
-  const adapter = new PrismaMariaDb(connectionUrl, {
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    port: Number(process.env.DATABASE_PORT),
+    connectionLimit: 5,
   });
 
   return new PrismaClient({ adapter });
